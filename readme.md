1b694705f0671ac0b4c25cb62eaa3ca7d8b8cf5e
Emil Pirfält <emil.pirfalt@gmail.com>
2021-12-29 13:04:38 +0100

    Use new version

--- readme.md
+++ readme.md
@@ -4,6 +4,7 @@
 
 - [MDN Service Worker](https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers)
 - [Stuff I wish I'd known sooner about service workers](https://gist.github.com/Rich-Harris/fd6c3c73e6e707e312d7c5d7d0f3b2f9)
+- [Offline Cookbook](https://web.dev/offline-cookbook/)
 - [Introduction to Service Worker](https://developers.google.com/web/ilt/pwa/introduction-to-service-worker)
 - [100 second youtube introduction](https://youtu.be/sFsRylCQblw?list=PLLHiuVbsSGYrq8kP6lNV5BXOYfe3PBCCO)
 - [Talk by Jake Archibald](https://youtu.be/cmGr0RszHc8?list=PLLHiuVbsSGYrq8kP6lNV5BXOYfe3PBCCO)
@@ -12,6 +13,21 @@
 - [MDN HTTP caching](https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers)
 - [Caching best practices & max-age gotchas](https://jakearchibald.com/2016/caching-best-practices/)
 
+## Running
+
+### Prerequisite
+
+```
+npm install -g serve
+```
+
+### Start
+
+```
+serve -l 8000 .
+open http://localhost:8000/
+```
+
 ## Gotchas
 
 > Turns out that when you call `navigator.serviceWorker.register('service-worker.js)` the request for `service-worker.js` _isn't_ intercepted by any service worker's `fetch` event handler.
@@ -19,3 +35,17 @@
 Since _that_ request is not handled by the service worker the service worker itself is always requested from the server. Which leads to at least one request to the server _every_ time, completely negating the offline capability 😭.
 
 However _that_ special request _does_ respect the http caching headers. So that "completely negating the offline capability" is not actually true 😅. But rather, offline capability only works for as long as the service worker does not need to be re-fetched. Meaning at most until the cache time expires or the file is evicted by the browser.
+
+## TODO
+
+- [x] Introduction and background.
+- [x] Watch [100 second youtube introduction](https://youtu.be/sFsRylCQblw?list=PLLHiuVbsSGYrq8kP6lNV5BXOYfe3PBCCO).
+- [ ] Offline support.
+- [ ] Manifest.
+- [ ] App idea choice.
+- [ ] UX experimentation.
+  - [ ] How do you "install" a PWA? Is it good enough?
+- [ ] Support "research".
+- [ ] Forms, 100% client side.
+- [ ] Storage alternatives?
+- [ ] Camera support?
---
+++ service-worker.js
@@ -0,0 +1,111 @@
+const CURRENT_CACHE = "two-v1";
+
+debug("sw self", self);
+
+function log(...args) {
+  console.log(new Date().toISOString(), CURRENT_CACHE, ...args);
+}
+function debug(...args) {
+  console.debug(new Date().toISOString(), CURRENT_CACHE, ...args);
+}
+function info(...args) {
+  console.info(new Date().toISOString(), CURRENT_CACHE, ...args);
+}
+function warn(...args) {
+  console.warn(new Date().toISOString(), CURRENT_CACHE, ...args);
+}
+
+self.addEventListener("install", (event) => {
+  // debugger;
+  debug("install", event);
+  event.waitUntil(
+    caches.open(CURRENT_CACHE).then((cache) => {
+      return cache.addAll([
+        "/", // /index.html
+        "/style.css",
+        "/favicon.ico",
+        "/checkbox.svg",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  // debugger;
+  debug("activate", event);
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      const toDelete = keyList.filter((k) => k != CURRENT_CACHE);
+      const deletePromises = toDelete.map((key) => caches.delete(key));
+      return Promise.all(deletePromises);
+    })
+  );
+});
+
+self.addEventListener("message", (event) => {
+  debugger;
+  debug("message", event);
+  // log("self.skipWaiting()", await self.skipWaiting());
+});
+
+self.addEventListener("fetch", (event) => {
+  // debugger;
+  const request = event.request; //new Request()
+  debug("fetch", request.method, new URL(request.url).pathname, event);
+  event.respondWith(serverFirstAndRefresh(request));
+});
+
+self.addEventListener("sync", async (event) => {
+  debug("sync", event);
+});
+
+self.addEventListener("push", async (event) => {
+  debug("push", event);
+});
+
+async function cacheOnly(request) {
+  const currCache = await caches.open(CURRENT_CACHE);
+  const cacheResponse = currCache.match(request);
+  debug("cache", cacheResponse);
+  return cacheResponse;
+}
+
+async function cacheFirstAndRefresh(request) {
+  const currCache = await caches.open(CURRENT_CACHE);
+
+  const cacheResponse = currCache.match(request);
+  const onlineResponse = fetchAndRefresh(request, currCache);
+
+  const response = (await cacheResponse) ?? (await onlineResponse);
+
+  debug("cache", cacheResponse);
+  debug("online", onlineResponse);
+  debug("actual", response);
+  return response;
+}
+
+async function serverFirstAndRefresh(request) {
+  const currCache = await caches.open(CURRENT_CACHE);
+
+  const cacheResponse = currCache.match(request);
+  const onlineResponse = fetchAndRefresh(request, currCache);
+
+  const response = (await onlineResponse) ?? (await cacheResponse);
+
+  debug("cache", cacheResponse);
+  debug("online", onlineResponse);
+  debug("actual", response);
+  return response;
+}
+
+async function fetchAndRefresh(request, cache) {
+  try {
+    const onlineResponse = await fetch(request);
+    const resClone = response.clone();
+    cache.put(request, resClone);
+    return onlineResponse;
+  } catch (err) {
+    debug("fetch error", err);
+    return undefined;
+  }
+}
