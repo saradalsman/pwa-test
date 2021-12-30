60429237ff0973bba351a5ac4fc4fbcf7a0af1bb
Emil Pirfält <emil.pirfalt@gmail.com>
2021-12-30 10:05:24 +0100

    Move static assets to /public

--- src/main.tsx
+++ src/main.tsx
@@ -9,3 +9,20 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./service-worker.js", {})
+    .then((reg) => {
+      // registration worked
+      // debugger;
+      console.log("Registration succeeded. Scope is " + reg.scope);
+    })
+    .catch((error) => {
+      // registration failed
+      debugger;
+      console.log("Registration failed with " + error);
+    });
+} else {
+  console.error("Wont work!");
+}
